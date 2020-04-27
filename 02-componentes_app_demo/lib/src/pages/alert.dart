62d3f38a79e1fb313678badeb1b6910df0752744
david martin perez <davidmartinperez1@gmail.com>
2020-04-27 16:21:04 +0200

    creado un dialog alert

--- 02-componentes_app_demo/lib/src/pages/alert.dart
+++ 02-componentes_app_demo/lib/src/pages/alert.dart
@@ -9,12 +9,55 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alerts'),
       ),
+      body: Center(
+        child: RaisedButton(
+          padding: EdgeInsets.symmetric(horizontal: 30),
+          child: Text('Mostrar alerta'),
+          color: Colors.blue[300],
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+          onPressed: () => _showAlert(context),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.exit_to_app),
         onPressed: () {
-          Navigator.pop( context );
+          Navigator.pop(context);
         },
       ),
     );
+  }
+
+  void _showAlert(context) {
+    showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(15)),
+            title: Text('Titulo alerta'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text('Contenido de la alerta'),
+                FlutterLogo(
+                  size: 100,
+                ),
+              ],
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Cancelar'),
+                onPressed: () => Navigator.of(context).pop(),
+                shape: StadiumBorder(),
+              ),
+              FlatButton(
+                child: Text('Ok'),
+                onPressed: () => Navigator.of(context).pop(),
+                shape: StadiumBorder(),
+              )
+            ],
+          );
+        });
   }
 }
